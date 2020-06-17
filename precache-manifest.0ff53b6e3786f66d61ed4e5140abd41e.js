7655e8a073c33a481b21cf3216cdfb3a1c153c36
nardos <32473919+nardost@users.noreply.github.com>
2020-06-17 01:02:03 -0500

    Updates

---
+++ precache-manifest.0ff53b6e3786f66d61ed4e5140abd41e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3793944103edf6cfd95204f28420e0bc",
+    "url": "/akaki/index.html"
+  },
+  {
+    "revision": "017e794c47cbeb138b88",
+    "url": "/akaki/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "34189c1ab60ddcff415e",
+    "url": "/akaki/static/js/2.af9a9620.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/akaki/static/js/2.af9a9620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017e794c47cbeb138b88",
+    "url": "/akaki/static/js/main.27d95a81.chunk.js"
+  },
+  {
+    "revision": "bf7e51f2d54f527b0170",
+    "url": "/akaki/static/js/runtime-main.7abc651b.js"
+  },
+  {
+    "revision": "2565541e228450ab736bf799d4682c81",
+    "url": "/akaki/static/media/akaki.2565541e.jpg"
+  },
+  {
+    "revision": "796b4b1e745f3fc2446ebb531f3bc2ff",
+    "url": "/akaki/static/media/woman.796b4b1e.png"
+  }
+]);
